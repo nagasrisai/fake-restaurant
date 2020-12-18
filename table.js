a5945d704dedf2924d6487b10725a24af681599c
EWcode-LA <seth.miller87@proton.mail>
2020-12-17 21:07:21 -0800

    jquery

--- table.js
+++ table.js
@@ -75,24 +75,24 @@
 			});
 	}
 
-	// This function resets all of the data in our tables. This is intended to let you restart a demo.
-	function clearTable(){
+	// // This function resets all of the data in our tables. This is intended to let you restart a demo.
+	// function clearTable(){
 
-		const currentURL = window.location.origin;
-		$.ajax({url: currentURL + "/api/clear", method: "POST"})
+	// 	const currentURL = window.location.origin;
+	// 	$.ajax({url: currentURL + "/api/clear", method: "POST"})
 
-	}
+	// }
 
-	$("#clear").on('click', function(){
-		alert("Clearing...");
-		clearTable();
+	// $("#clear").on('click', function(){
+	// 	alert("Clearing...");
+	// 	clearTable();
 
-		// Refresh the page after data is cleared
-		location.reload();
-	})
+	// 	// Refresh the page after data is cleared
+	// 	location.reload();
+	// })
 
 
-	// Run Queries!
-	// ==========================================
-	runTableQuery();
-	runWaitListQuery();
+	// // Run Queries!
+	// // ==========================================
+	// runTableQuery();
+	// runWaitListQuery();
